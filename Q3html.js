89c8f8f88b71abbdc0f4826b4445de87a82a092f
luana082 <lls40@aluno.ifal.edu.br>
2024-12-06 08:48:12 -0300

    Q3html.js

---
+++ Q3html.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+
+‎scripts.js
+
+let nome = prompt("imforme o seu nome:");
+for (let i = 0; i < 10; i++) {
+    alert(nome);
+}
