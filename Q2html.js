e5e99233b7cbd46cbdcb75985075f1af21f5255e
luana082 <lls40@aluno.ifal.edu.br>
2024-12-06 08:46:43 -0300

    Q2html.js
    o js esta junto

---
+++ Q2html.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+
+
+‎scripts.js
+
+let Soma = 0;
+for (let i = 1; i <= 15; i++) {
+    Soma += i;
+}
+alert("soma dos números de 1 a 15 é: " + Soma);
