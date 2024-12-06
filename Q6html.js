1158d7cd2f56508fdee1a2dd98cda2be39ddea4e
luana082 <lls40@aluno.ifal.edu.br>
2024-12-06 08:51:52 -0300

    Q6html.js

---
+++ Q6html.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+</head>
+<body>
+    <script src="Js/scripts.js"></script>
+</body>
+</html>
+‎
+
+let somaidades = 0;
+for (let i = 1; i <= 20; i++) {
+    let idade = parseInt(prompt('imforme a idade da ' + i + ' pessoa:'));
+    
+    soma idades += idade;
+}
+alert(' soma das idades é: ' + somaidades);
